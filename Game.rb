a9b9116aff8be6bb1836f04369a9d29f2d1fc723
Lapatch77 <Lukeapatch@gmail.com>
2021-06-01 19:25:44 +1000

    added story.rb to store strings and forks.

--- Game.rb
+++ Game.rb
@@ -7,7 +7,7 @@ require 'pastel'
 require 'thor'
 require 'equatable'
 require 'artii'
-#require_relative 'story'
+require_relative 'story'
 
 # class Game
 
@@ -92,34 +92,7 @@ require 'artii'
 
     #def initialize(id, story, *Paragraphs)
     
-    paragraph_1 = def paragraph_1 
-      puts "Ah the north road, a wise choice friend as your way will meander towards Ered Mithrin and Durins folk of the Grey Mountains'
-  'Perhaps like Gandalf and his companions who whence this way you will find Erebor the lonely mountain, whereby Smaug the Terrible met his end.'
-  'Now friend the hour grows late and you best away lest you not find a clearing within the woods to light a fire for camp at night.
-  'Bidding the kind old man well and flicking a silver coin his way you nudge your horse with your knees and head towards the woods.'
-  'As you approach the branchs of the dark and twisted trees seem to reach out and their trunks rear up and lean forward to engulf you. Soon the space'
-  'between the boughs becomes thinner and thinner and although what seemed like moments ago late afternoon now seems like a twighlight under the weight of the ancient forest."
-  
-  end 
-  
-  paragraph_2 = def paragraph_2 
-      puts "Ah the north road, a wise choice friend as your way will meander towards Ered Mithrin and Durins folk of the Grey Mountains'
-  'Perhaps like Gandalf and his companions who whence this way you will find Erebor the lonely mountain, whereby Smaug the Terrible met his end.'
-  'Now friend the hour grows late and you best away lest you not find a clearing within the woods to light a fire for camp at night.
-  'Bidding the kind old man well and flicking a silver coin his way you nudge your horse with your knees and head towards the woods.'
-  'As you approach the branchs of the dark and twisted trees seem to reach out and their trunks rear up and lean forward to engulf you. Soon the space'
-  'between the boughs becomes thinner and thinner and although what seemed like moments ago late afternoon now seems like a twighlight under the weight of the ancient forest."
-  end 
-  
-  paragraph_3 = def paragraph_3 
-      puts "Ah the north road, a wise choice friend as your way will meander towards Ered Mithrin and Durins folk of the Grey Mountains'
-  'Perhaps like Gandalf and his companions who whence this way you will find Erebor the lonely mountain, whereby Smaug the Terrible met his end.'
-  'Now friend the hour grows late and you best away lest you not find a clearing within the woods to light a fire for camp at night.
-  'Bidding the kind old man well and flicking a silver coin his way you nudge your horse with your knees and head towards the woods.'
-  'As you approach the branchs of the dark and twisted trees seem to reach out and their trunks rear up and lean forward to engulf you. Soon the space'
-  'between the boughs becomes thinner and thinner and although what seemed like moments ago late afternoon now seems like a twighlight under the weight of the ancient forest."
-  
-  end
+    
   
 
 end
---
+++ Story.rb
@@ -0,0 +1,89 @@
+paragraph_1 = def paragraph_1 
+      puts "Ah the north road, a wise choice friend as your way will meander towards Ered Mithrin and Durins folk of the Grey Mountains'
+  'Perhaps like Gandalf and his companions who whence this way you will find Erebor the lonely mountain, whereby Smaug the Terrible met his end.'
+  'Now friend the hour grows late and you best away lest you not find a clearing within the woods to light a fire for camp at night.
+  'Bidding the kind old man well and flicking a silver coin his way you nudge your horse with your knees and head towards the woods.'
+  'As you approach the branchs of the dark and twisted trees seem to reach out and their trunks rear up and lean forward to engulf you. Soon the space'
+  'between the boughs becomes thinner and thinner and although what seemed like moments ago late afternoon now seems like a twighlight under the weight of the ancient forest."
+
+  "As you walk deeper into the dark woods you hear a rustling and from the corner of your eye you see movement to your left"
+
+  prompt.select("Do you stop to listen? Do you leave the path to investigate? or do you spur your horse on faster?") do |menu|
+    menu.choice "Stop to listen", -> {paragraph_4}
+    menu.choice "Leave path and investigate", -> {paragraph_5}
+    menu.choice "Spur horse on faster", -> {paragraph_6}
+  
+  end 
+  
+  paragraph_2 = def paragraph_2 
+      puts "  'Ah the middle way, a sensible choice friend as your way will take you through the lands of the Sindarin or Silvan Elves and Thranduil's realm'
+  'Now friend the hour grows late and you best away lest you not find a clearing within the woods to light a fire for camp at night.
+  'Bidding the kind old man well and flicking a silver coin his way you nudge your horse with your knees and head towards the woods.'
+  'As you approach the branchs of the dark and twisted trees seem to reach out and their trunks rear up and lean forward to engulf you. Soon the space'
+  'between the boughs becomes thinner and thinner and although what seemed like moments ago late afternoon now seems like a twighlight under the weight of the ancient forest."
+  ""
+  "As you walk deeper into the dark woods you hear a rustling and from the corner of your eye you see movement to your left"
+
+  prompt.select("Do you stop to listen? Do you leave the path to investigate? or do you spur your horse on faster?") do |menu|
+    menu.choice "Stop to listen", -> {paragraph_4}
+    menu.choice "Leave path and investigate", -> {paragraph_5}
+    menu.choice "Spur horse on faster", -> {paragraph_6}
+  end 
+  
+  paragraph_3 = def paragraph_3 
+      puts "Ah the southern road, be wary friend of things that crawl and climb with many legs and which weave webs which which to catch their prey'
+  'Now friend the hour grows late and you best away lest you not find a clearing within the woods to light a fire for camp at night.
+  'Bidding the kind old man well and flicking a silver coin his way you nudge your horse with your knees and head towards the woods.'
+  'As you approach the branchs of the dark and twisted trees seem to reach out and their trunks rear up and lean forward to engulf you. Soon the space'
+  'between the boughs becomes thinner and thinner and although what seemed like moments ago late afternoon now seems like a twighlight under the weight of the ancient forest."
+
+  "As you walk deeper into the dark woods you hear a rustling and from the corner of your eye you see movement to your left"
+
+end
+
+#   prompt.select("Do you stop to listen? Do you leave the path to investigate? or do you spur your horse on faster?") do |menu|
+#     menu.choice "Stop to listen", -> {paragraph_4}
+#     menu.choice "Leave path and investigate", -> {paragraph_5}
+#     menu.choice "Spur horse on faster", -> {paragraph_6}
+
+#   end
+  
+# #   end
+
+#   paragraph_4 = def paragraph_4
+#       puts "Ah the southern road, be wary friend of things that crawl and climb with many legs and which weave webs which which to catch their prey'
+#   'Now friend the hour grows late and you best away lest you not find a clearing within the woods to light a fire for camp at night.
+#   'Bidding the kind old man well and flicking a silver coin his way you nudge your horse with your knees and head towards the woods.'
+#   'As you approach the branchs of the dark and twisted trees seem to reach out and their trunks rear up and lean forward to engulf you. Soon the space'
+#   'between the boughs becomes thinner and thinner and although what seemed like moments ago late afternoon now seems like a twighlight under the weight of the ancient forest."
+  
+#   end
+
+#   paragraph_5 = def paragraph_5
+#       puts "Ah the southern road, be wary friend of things that crawl and climb with many legs and which weave webs which which to catch their prey'
+#   'Now friend the hour grows late and you best away lest you not find a clearing within the woods to light a fire for camp at night.
+#   'Bidding the kind old man well and flicking a silver coin his way you nudge your horse with your knees and head towards the woods.'
+#   'As you approach the branchs of the dark and twisted trees seem to reach out and their trunks rear up and lean forward to engulf you. Soon the space'
+#   'between the boughs becomes thinner and thinner and although what seemed like moments ago late afternoon now seems like a twighlight under the weight of the ancient forest."
+  
+#   end
+
+#   paragraph_6 = def paragraph_6
+#       puts "Ah the southern road, be wary friend of things that crawl and climb with many legs and which weave webs which which to catch their prey'
+#   'Now friend the hour grows late and you best away lest you not find a clearing within the woods to light a fire for camp at night.
+#   'Bidding the kind old man well and flicking a silver coin his way you nudge your horse with your knees and head towards the woods.'
+#   'As you approach the branchs of the dark and twisted trees seem to reach out and their trunks rear up and lean forward to engulf you. Soon the space'
+#   'between the boughs becomes thinner and thinner and although what seemed like moments ago late afternoon now seems like a twighlight under the weight of the ancient forest."
+  
+#   end
+
+
+
+# end
+
+# # end
+
+# # end
+end
+
+end
