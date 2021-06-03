cb57ebb4b6a88ae7e4667f3bf0221858c757dad2
Lapatch77 <Lukeapatch@gmail.com>
2021-06-03 19:01:50 +1000

    changed text

--- Story9.rb
+++ Story9.rb
@@ -6,7 +6,7 @@ puts ""
 puts "" 
 puts "  Spurring your horse forward you continue along the path. "
 puts ""
-puts "  Suddenly a goblin leaps screaming from the brush and burys a black blade in your heart."
+puts "  Suddenly a goblin leaps screaming from the brush, your horse rears up and you hit your head on a branch. Falling from your mount the goblin stand over you and and burys a black blade in your heart."
 puts "" 
 puts "  YOU HAVE DIED"
 puts font.write("GAME OVER",letter_spacing: 4)
